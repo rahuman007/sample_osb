e01a9228283f50cf108d2dbbfb7a537d188a5f79
fisurrahman.h <fisurrahman.h@ptpll238.appviewx.com>
2020-07-08 21:53:31 +0530

    changes to build war

--- src/main/java/com/sample/service/broker/ServiceBrokerMainApplication.java
+++ src/main/java/com/sample/service/broker/ServiceBrokerMainApplication.java
@@ -2,6 +2,7 @@ package com.sample.service.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ServiceBrokerMainApplication {
@@ -10,4 +11,8 @@ public class ServiceBrokerMainApplication {
 		SpringApplication.run(ServiceBrokerMainApplication.class, args);
 	}
 
+	 @RequestMapping("hello")
+	 public String sayHello(){
+	        return ("Hello, SpringBoot on Wildfly");
+	 }
 }
